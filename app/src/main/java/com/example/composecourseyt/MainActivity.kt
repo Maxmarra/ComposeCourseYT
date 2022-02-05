ab12e3519dbf04ae78b793a614eac16cbaa3d15a
Maxmarra <5919669@gmail.com>
2022-02-05 18:59:20 +0300

    Image Card

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -1,41 +1,118 @@
 package com.example.composecourseyt
 
+import android.icu.text.CaseMap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Colors
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composecourseyt.ui.theme.ComposeCourseYTTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier
-                    .background(Color.Green)
-                    .fillMaxWidth()
-                    .fillMaxHeight()
-                    .padding(50.dp)
-                    .border(5.dp, Color.Blue)
 
-            ){
-                Text("One", modifier = Modifier.border(10.dp, Color.Magenta)
-                    .fillMaxWidth())
-                Spacer(modifier = Modifier.height(50.dp))
-                Text("Two")
-            }
+ImageCard(
+    painter = painterResource(id = R.drawable.dice_6),
+    contentDescription = "Dice",
+    title = "Dice here" )
         }
     }
 }
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+){
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(10.dp),
+        elevation = 5.dp
+    ) {
+        Box(
+            modifier = Modifier.height(400.dp)
+                .padding(16.dp)
+
+        ){
+            Image(
+                modifier = Modifier.fillMaxWidth(),
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Fit,
+                alignment = Alignment.Center
+            )
+
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 800f
+
+                    )
+                )
+            )
+            {
+
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart)
+            {
+               Text(
+                   title,
+                   style = TextStyle(
+                       color = Color.White,
+                       fontSize = 16.sp
+                   )
+               )
+                
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
