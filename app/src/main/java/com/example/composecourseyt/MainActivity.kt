8e6405c21494da396de48712ae5f95fc095001b5
Maxmarra <5919669@gmail.com>
2022-02-05 18:18:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.composecourseyt
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Colors
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composecourseyt.ui.theme.ComposeCourseYTTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(
+                modifier = Modifier
+                    .background(Color.Green)
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .padding(50.dp)
+                    .border(5.dp, Color.Blue)
+
+            ){
+                Text("One", modifier = Modifier.border(10.dp, Color.Magenta)
+                    .fillMaxWidth())
+                Spacer(modifier = Modifier.height(50.dp))
+                Text("Two")
+            }
+        }
+    }
+}
+
