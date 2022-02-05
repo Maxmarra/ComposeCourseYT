6e73f4e76cceb5c4b96d10891f9d46d432432c82
Maxmarra <5919669@gmail.com>
2022-02-05 20:51:38 +0300

    Styling text

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.composecourseyt
 
+import android.graphics.Color
+import android.graphics.fonts.FontFamily
 import android.icu.text.CaseMap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -19,85 +21,40 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composecourseyt.ui.theme.ComposeCourseYTTheme
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-ImageCard(
-    painter = painterResource(id = R.drawable.dice_6),
-    contentDescription = "Dice",
-    title = "Dice here" )
-        }
-    }
-}
-
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-){
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(10.dp),
-        elevation = 5.dp
-    ) {
-        Box(
-            modifier = Modifier.height(400.dp)
-                .padding(16.dp)
-
-        ){
-            Image(
-                modifier = Modifier.fillMaxWidth(),
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Fit,
-                alignment = Alignment.Center
-            )
-
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    Brush.verticalGradient(
-                        colors = listOf(
-                            Color.Transparent,
-                            Color.Black
-                        ),
-                        startY = 800f
-
-                    )
-                )
-            )
-            {
-
-            }
-
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomStart)
-            {
-               Text(
-                   title,
-                   style = TextStyle(
-                       color = Color.White,
-                       fontSize = 16.sp
-                   )
-               )
-                
-            }
-        }
+                    .background(Color(0xFF101010))
+            ) {
+                Text(
+                    text = "JetPack Compose",
+                    color = Color.White,
+                    fontSize = 15.sp,
+                    fontFamily = FontFamily.Monospace,
+                    textDecoration = TextDecoration.LineThrough
+                ) {
 
+                }
+            }
+
+        }
     }
 }
+
+
 
 
 
