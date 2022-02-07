52a2380d0e311fb997b1b763bb878ac4da304aac
Maxmarra <5919669@gmail.com>
2022-02-07 18:45:38 +0300

    Snackbar Textfield Button

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -1,58 +1,80 @@
 package com.example.composecourseyt
 
-import android.graphics.Color
-import android.graphics.fonts.FontFamily
-import android.icu.text.CaseMap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.ModifierInfo
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.composecourseyt.ui.theme.ComposeCourseYTTheme
-import org.w3c.dom.Text
+import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color(0xFF101010))
-            ) {
-                Text(
-                    text = "JetPack Compose",
-                    color = Color.White,
-                    fontSize = 15.sp,
-                    fontFamily = FontFamily.Monospace,
-                    textDecoration = TextDecoration.LineThrough
-                ) {
 
-                }
+            val scaffoldState = rememberScaffoldState()
+            //делегируем с by, поэтому в textFieldState
+            //уже сразу строка
+            var textFieldState by remember{
+                mutableStateOf("")
             }
+            val scope = rememberCoroutineScope()
 
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                scaffoldState = scaffoldState
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 30.dp)
+                ) {
+                    TextField(
+                        value = textFieldState,
+                        label = {
+                        Text("Enter your name")
+                    },
+                      onValueChange = {
+                          textFieldState = it
+                      },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                   Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = {
+                        scope.launch {
+                            scaffoldState
+                                .snackbarHostState
+                                .showSnackbar("Hello $textFieldState")
+                        }
+                    }
+                    )
+                    {
+                        Text("please greet me")
+                    }
+                }
+
+
+            }
         }
     }
 }
+
+
 
 
 
